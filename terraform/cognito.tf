7de0825eb7242d88de2088aa69c76115f123f3d1
Mateus Sales Albino <mateus3009@gmail.com>
2024-07-26 17:27:30 -0300

    build: adding cognito

---
+++ terraform/cognito.tf
@@ -0,0 +1,50 @@
+resource "aws_cognito_user_pool" "user_pool" {
+    name = "healthmed"
+
+    admin_create_user_config {
+        allow_admin_create_user_only = true
+    }
+
+    schema {
+        attribute_data_type = "String"
+        name                = "CRM"
+        required            = false
+    }
+
+    schema {
+        attribute_data_type = "String"
+        name                = "CPF"
+        required            = false
+
+        string_attribute_constraints {
+            min_length = 11
+            max_length = 11
+        }
+    }
+
+    schema {
+        attribute_data_type = "String"
+        name                = "CUSTOMER_ID"
+        required            = false
+    }
+
+}
+
+resource "aws_cognito_user_group" "doctor" {
+    name         = "doctor"
+    user_pool_id = aws_cognito_user_pool.user_pool.id
+}
+
+resource "aws_cognito_user_group" "patient" {
+    name         = "patient"
+    user_pool_id = aws_cognito_user_pool.user_pool.id
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+    name         = "client"
+    user_pool_id = aws_cognito_user_pool.user_pool.id
+}
+
+output "user_pool_id" {
+    value = aws_cognito_user_pool.user_pool.id
+}
